c4cd41f0e20bf583fc5f9f411b85e5fce9733ec3
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-15 14:06:41 -0700

    the server been tested

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require("express");
+const app = express();
+const useServer = require("./server");
+
+const port = 4000;
+
+app.use(express.json());
+app.use("/", useServer);
+
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}`);
+});
